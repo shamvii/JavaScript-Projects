a39e66e59197b75c7713acd96ea17b712b15f835
Shambhavi Shukla <shambhavishukla24@gmail.com>
2017-03-16 20:55:34 -0400

    Add simplequiz project

---
+++ simplequiz/js/script.js
@@ -0,0 +1,20 @@
+function submitAnswers(form) {
+	var total = 5;
+	var score = 0;
+
+	//Get User Input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+
+    alert(q2);
+	///Validation
+	// if(q1 == null || q1 = '') {
+	// 	alert('You missed question 1');
+	// 	return false;
+	// }
+
+	return false;
+}
